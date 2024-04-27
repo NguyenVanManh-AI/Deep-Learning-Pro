a48d2bf82afb1d2907bbb6dd6ed85cffed715b6d
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-04-27 16:26:56 +0700

    App

--- App/Model/ai_project/ai_project/settings.py
+++ App/Model/ai_project/ai_project/settings.py
@@ -29,9 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',
-]
+# CORS_ALLOWED_ORIGINS = ['http://localhost:8080',]
+CORS_ALLOWED_ORIGINS = ["*"]
 
 CORS_ALLOW_METHODS = [
     'GET',
---
+++ App/README.md
@@ -0,0 +1,2 @@
+# Advanced artificial intelligence - Step by step .
+
